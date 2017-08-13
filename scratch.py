Fixed normalization method

scratch.py
@@ -1,15 +1,10 @@
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+from ArrayNormalizer import ArrayNormalizer
 
-arr = [[0,1,3],
-        [4,5,6]]
+arr1 = [ [[0,2],[3,4]], [[5,6],[7,8]], [[6,8],[9,10]], [[12,15],[7,3]] ]
+arr1 =np.asarray(arr1)
 
-arr = np.asarray(arr)
 
-arr = (np.hstack(arr))
-mmscaler = MinMaxScaler(feature_range=(0, 1))
-#
-# print(arr)
-# print()
-#
-print((mmscaler.fit_transform(arr)))
+norm = ArrayNormalizer(arr1)
+print(norm.transform(arr1))

test.py
@@ -16,9 +16,12 @@ from sklearn.preprocessing import Normalizer
 from keras.layers.advanced_activations import LeakyReLU
 from sklearn.preprocessing import MinMaxScaler
 import copy
+from keras import optimizers
+from ArrayNormalizer import ArrayNormalizer
 
 
 
+#gets stock data from google finance, excludes some columns, then returns a dataframe
 def get_stock_data(stock_name, normalized=0):
     print("GETTING STOCK DATA")
     url = "http://www.google.com/finance/historical?q=" + stock_name + "&startdate=Jul+12%2C+2003&enddate=Jul+11%2C+2017&num=30&ei=rCtlWZGSFN3KsQHwrqWQCw&output=csv"
@@ -30,13 +33,16 @@ def get_stock_data(stock_name, normalized=0):
     return df
 
 
-def load_data(stock, seq_len, file, train_percent=.75):
+
+#Loads in stock data from a dataframe and tra
+def load_data(stock, seq_len, train_percent=.75):
     data = stock.as_matrix()
 
     # iterate so that we can also capture a sequence for a target
     sequence_length = seq_len + 1
 
     print("SPLITTING INTO TIMESERIES")
+
     # segment the data into timeseries (these will be overlapping)
     result = []
     for index in range(len(data) - sequence_length):
@@ -45,6 +51,12 @@ def load_data(stock, seq_len, file, train_percent=.75):
 
     result = np.array(result)
 
+    # normalize
+    reference_points = [] # for de-normalizing outside of the function
+    for i in range(0, len(result)):
+        reference_points.append(result[i][0][0])
+        result[i] = (((result[i]) / (result[i][0][0])) - 1)
+
     # train test split
     row = round(train_percent * result.shape[0])
     train = result[:int(row), :]
@@ -52,28 +64,31 @@ def load_data(stock, seq_len, file, train_percent=.75):
 
     x_train = train[:, :-1]
     train_target_timeseries = train[:, -1, -1]
-    y_train = []
+    y_train = train_target_timeseries
 
     x_test = test[:, :-1]
     test_target_timeseries = test[:, -1, -1]
-    y_test = []
+    y_test = test_target_timeseries
 
-    for i in range(0, len(train_target_timeseries)):
-        start_price = x_train[i][-1][2]
-        percent_increase = ((train_target_timeseries[i] - start_price) / start_price) * 100
-        y_train.append(percent_increase)
 
-    for i in range(0, len(test_target_timeseries)):
-        start_price = x_test[i][-1][2]
-        percent_increase = ((test_target_timeseries[i] - start_price) / start_price) * 100
-        y_test.append(percent_increase)
+    #In case we want to train on percent increase rather than a stock value
+    # for i in range(0, len(train_target_timeseries)):
+    #     start_price = x_train[i][-1][2]
+    #     percent_increase = ((train_target_timeseries[i] - start_price) / start_price) * 100
+    #     y_train.append(percent_increase)
+    #
+    # for i in range(0, len(test_target_timeseries)):
+    #     start_price = x_test[i][-1][2]
+    #     percent_increase = ((test_target_timeseries[i] - start_price) / start_price) * 100
+    #     y_test.append(percent_increase)
+
 
     x_train = np.asarray(x_train)
     x_test = np.asarray(x_test)
     y_train = np.asarray(y_train)
     y_test = np.asarray(y_test)
 
-    return [x_train, y_train, x_test, y_test]
+    return [x_train, y_train, x_test, y_test, reference_points]
 
 
 def build_model(layers):
@@ -84,46 +99,29 @@ def build_model(layers):
     model.add(LSTM(64, input_shape=(layers[1], layers[0]), return_sequences=False))
     model.add(Dropout(d))
     model.add(Dense(16, kernel_initializer="uniform"))
-    model.add(Activation('tanh'))
+    model.add(LeakyReLU(alpha=0.3))
     model.add(Dense(1, kernel_initializer="uniform"))
     model.add(LeakyReLU(alpha=0.3))
-    model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
+    #adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    model.compile(loss='mse', optimizer="rmsprop", metrics=['accuracy'])
     return model
 
 
+
 # MAIN()
-file = open("rnn_output.txt", "w")
 
 stock_name = 'GOOGL'
 df = get_stock_data(stock_name, 0)
 
-# df['High'] = df['High'] / 1000
-# df['Open'] = df['Open'] / 1000
-# df['Close'] = df['Close'] / 1000
 
-window = 5
-X_train, y_train, X_test, y_test = load_data(df[::-1], window, file, train_percent=.9)
-
-X_train_unprocessed = np.copy(X_train)
-X_test_unprocessed = np.copy(X_test)
-
-#scale values down for lstm
-mmscaler = MinMaxScaler(feature_range=(0, 1))
-
-for i in range(0, len(X_train)):
-    temp = np.hstack(X_train[i])
-    temp = (mmscaler.fit_transform(temp))
-    X_train[i] = np.split(temp, 5)
-
-for i in range(0, len(X_test)):
-    temp = np.hstack(X_test[i])
-    temp = (mmscaler.fit_transform(temp))
-    X_test[i] = np.split(temp, 5)
+window = 10
+X_train, y_train, X_test, y_test, ref = load_data(df[::-1], window, train_percent=.9)
 
 print("X_train", X_train.shape)
 print("y_train", y_train.shape)
 print("X_test", X_test.shape)
 print("y_test", y_test.shape)
+
 
 model = build_model([3, window, 1])
 
@@ -131,9 +129,10 @@ model.fit(
     X_train,
     y_train,
     batch_size=512,
-    epochs=300,
+    epochs=50,
     validation_split=0.1,
-    verbose=100)
+    verbose=2)
+
 
 trainScore = model.evaluate(X_train, y_train, verbose=100)
 print('Train Score: %.2f MSE (%.2f RMSE) (%.2f)' % (trainScore[0], math.sqrt(trainScore[0]), trainScore[1]))
@@ -143,6 +142,9 @@ print('Test Score: %.2f MSE (%.2f RMSE) (%.2f)' % (testScore[0], math.sqrt(testS
 
 p = model.predict(X_test)
 
+
+# document results in file
+file = open("rnn_output4.txt", "w")
 for i in range(0, len(X_train)):
     for s in range(0, window):
         file.write(str(X_train[i][s]) + "\n")
@@ -150,17 +152,19 @@ for i in range(0, len(X_train)):
     file.write("\n")
 
 for i in range(0, len(X_test)):
-    for s in range(0, 5):
+    for s in range(0, window):
         file.write(str(X_test[i][s]) + "\n")
     file.write("Target: " + str(y_test[i]) + "\n")
     file.write("Prediction: " + str(p[i]) + "\n")
     file.write("\n")
 
-for i in range(0, len(p)):
-    start_price = X_test_unprocessed[i][-1][2]
-    p[i] = (start_price/100 * p[i] + start_price)
-    y_test[i] = (start_price/100 * y_test[i] + start_price)
 
+# de-normalize
+for i in range(0, len(p)):
+    p[i] = (p[i] + 1) * ref[int(.9 * len(ref) + i)]
+    y_test[i] = (y_test[i] + 1 * ref[int(.9 * len(ref) + i)])
+
+# plot
 plt.plot(p, color='red', label='prediction')
 plt.plot(y_test, color='blue', label='y_test')
 plt.legend(loc='upper left')