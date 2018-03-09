021218fe424164a3e65deb3d09e23efbf23dc6db
Andrew Smith <als5ev@virginia.edu>
2018-03-08 22:17:32 -0500

    fixed arrow api change

--- Version2__twitter_sentiment/date_handler.py
+++ Version2__twitter_sentiment/date_handler.py
@@ -8,19 +8,25 @@ def dates_to_sentiment(dates, ticker, max_tweets):
 
     ticker = "$" + ticker
 
+    print(dates)
+
     sentiments = []
     for d in dates:
-        arrow_date = arrow.get(d)
+        arrow_date = arrow.get(d, "YYYY-MM-DD")
+
         tweetCriteria = got3.manager.TweetCriteria().setQuerySearch("{}{}".format("#", ticker)).setMaxTweets(max_tweets) \
-            .setSince(arrow_date.replace(days=1).format("YYYY-MM-DD")) \
-            .setUntil(arrow_date.replace(days=2).format("YYYY-MM-DD"))
+            .setSince(arrow_date.replace(days=-1).format("YYYY-MM-DD")) \
+            .setUntil(arrow_date.format("YYYY-MM-DD"))
         tweets = got3.manager.TweetManager.getTweets(tweetCriteria)
 
         sents_per_date = []
         for t in tweets:
+            #print(t.text)
             blob = TextBlob(t.text)
             sent = blob.sentiment[0] #get the polarity (subjectivity seems less important)
             sents_per_date.append(sent)
+
+        print("length: " + str(len(sents_per_date)))
 
         mean_sentiment = sum(sents_per_date) / len(sents_per_date)
 
@@ -41,8 +47,8 @@ def dates_to_sentiment(dates, ticker, max_tweets):
     return sentiments
 
 
-#  #UNIT TEST
-# dates = ['10-Aug-17', '11-Aug-17']
-# ticker = '$GOOGL'
-# max_tweets = 100
-# print(dates_to_sentiment(dates, ticker, max_tweets))
+ #UNIT TEST
+dates = ['2018-03-01']
+ticker = '$GOOGL'
+max_tweets = 500
+print(dates_to_sentiment(dates, ticker, max_tweets))
